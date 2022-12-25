e36b140618d03378a1587758644c8947dfd5f040
shahfahad19 <sfahad4280@gmail.com>
2022-12-25 13:40:40 +0500

    First commit

---
+++ src/Components/Login.js
@@ -0,0 +1,10 @@
+import React from'react';
+import ReactDOM from'react-dom';
+
+export function Login() {
+    return(
+        <div>
+            <div className='heading'>Login</div>
+        </div>
+    );
+}
